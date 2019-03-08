0e6e62015fab25b2c855f4954db850c3e84f9c5a
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-08 13:46:42 -0500

    added elements to login page and a little functionality

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVCViewController.swift
+//  Smack
+//
+//  Created by Austin Rightnowar on 3/8/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Austin Rightnowar on 3/8/19.
+//  Copyright © 2019 Austin Rightnowar. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
