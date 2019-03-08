d599dce273c9b8b325dd10e2d9c2a64d19747dc1
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-08 16:58:53 -0500

    Added create account VC and exit action

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginVCViewController.swift
+//  CreateAccountVC.swift
 //  Smack
 //
 //  Created by Austin Rightnowar on 3/8/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginVC: UIViewController {
+class CreateAccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,10 +17,7 @@ class LoginVC: UIViewController {
     }
     
     @IBAction func closePressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-    @IBAction func createAccountBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
+        performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
 
