3594e20bd0a71f728f21b6b75e9c21068a96144b
Austin Rightnowar <a.rightnowar24@gmail.com>
2019-03-10 20:36:02 -0400

    added rounded button features and created create account function to register user

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -58,6 +58,9 @@ class AuthService {
         Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
             }
         }
     }
